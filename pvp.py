7e5cf344a6ec050f6f771c809f8b0efee9e643dc
NishkalHundia <nishkalnhundia@gmail.com>
2022-12-31 16:27:30 +0530

    curr version

--- pvp.py
+++ pvp.py
@@ -13,10 +13,10 @@ def index():
 
 @app.route("/ajudge", methods = ["POST"])
 def ajudge():
-        #data = request.get_json()
-        ahere = 2
-        bhere = 1
-        return redirect(url_for("result"))
+    #data = request.get_json()
+    a = 2
+    b = 1
+    return redirect(url_for("result"))
 
 @app.route("/bjudge", methods = ["POST"])
 def bjudge():
@@ -26,23 +26,18 @@ def bjudge():
 
 @app.route("/result", methods = ["POST"])
 def result():
-    a = 1
-    b = 2
     d = 0
-    try:
-        while d<3:
-            if a != None and b != None:
-                if a > b:
-                    return "a"
-                elif a < b:
-                    return "b"
-            else:
-                time.sleep(1)
-                d+=1
-        return "0"
-    except:
-        return "0"
-
+    while d<3:
+        if a != None and b != None:
+            if a > b:
+                return "a"
+            elif a < b:
+                return "b"
+        else:
+            time.sleep(1)
+            d+=1
+    return "0"
+    
 @app.route("/leaderboard")
 def leaderboard():
     return lb

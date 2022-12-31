40295b3e76b751fdc0b09979c0670a980ea4e57f
Prateek <prateek.deshmukh19@gmail.com>
2023-01-01 01:06:18 +0530

    switched to alchemy

--- pvp.py
+++ pvp.py
@@ -40,9 +40,10 @@ def ajudge():
     global a
     a = int(data['score'])
     global num
-    num = cursor.execute("select No from score order by no desc limit 1")
-    cursor.execute("insert into score(No, A) values (%s, %s)", (num[0] + 1, a))
-    cursor.commit()
+    with mydb.commit() as conn:
+        num = conn.execute("select No from score order by no desc limit 1")
+        conn.execute("insert into score(No, A) values (%s, %s)", (num[0] + 1, a))
+        conn.commit()
     return
 
 @app.route("/bjudge", methods = ["POST"])
@@ -50,18 +51,26 @@ def bjudge():
     time.sleep(3)
     data = request.get_json()
     global b
-    b =  int(data["score"])
-    cursor.execute("insert into score (B) values (%s) where No = %s", (b, num))
-    cursor.commit()
+
+    with mydb.commit() as conn:
+        b = int(data['score'])
+        conn.execute("insert into score(B) values (%s) where No = %s", (b, num[0]+1))
+        conn.commit()
+
+    # b =  int(data["score"])
+    # cursor.execute("insert into score (B) values (%s) where No = %s", (b, num))
+    # cursor.commit()
     return redirect(url_for("result"))
 
 @app.route("/result", methods = ["POST","GET"])
 def result():
     if request.method == "POST":
-        num = cursor.execute("select No from score order by no desc limit 1")
-        P1 = int(cursor.execute("select A from score where No = %s", (num[0])))
-        P2 = int(cursor.execute("select B from score where No = %s", (num[0])))
-        cursor.commit()
+        with mydb.commit() as conn:
+            num = conn.execute("select No from score order by no desc limit 1")
+            P1 = int(conn.execute("select A from score where No = %s", (num[0])))
+            P2 = int(conn.execute("select B from score where No = %s", (num[0])))
+            conn.commit()
+
         if P1 > P2:
             return "a"
         elif P2 < P1:
